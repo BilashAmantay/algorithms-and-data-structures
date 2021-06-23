f706db0fc6a55b79abca5f14617b4744cbf85b35
aman <bilash.amantay@gmail.com>
2021-06-23 20:17:58 +0800

    Completed Linked List Cycle II

--- linked-list/linked-list-has-circle.py
+++ linked-list/linked-list-has-circle.py
@@ -1,22 +1,22 @@
 class Node:
-    def __init__(self,val,next=None):
+    def __init__(self, val, next=None):
         self.val = val
-        self.next=next
+        self.next = next
 
 
 class MyLinkedList:
 
-    def __init__(self,next=None):
+    def __init__(self, next=None):
         self.Head = Node(None)
         """
         Initialize your data structure here.
         """
-        
+
     def get(self, index: int):
 
         index_count = 0
         return_this = -1
-        
+
         if self.Head != None:
 
             current = self.Head
@@ -27,18 +27,17 @@ class MyLinkedList:
                     return_this = current.val
                     return current.val
 
-                index_count +=1
+                index_count += 1
 
                 if current.next == None:
                     break
-                
+
                 current = current.next
         return return_this
-                
+
         """
         Get the value of the index-th node in the linked list. If the index is invalid, return -1.
         """
-        
 
     def addAtHead(self, val: int):
 
@@ -53,18 +52,17 @@ class MyLinkedList:
 
         self.Head = Node(val, old_Head)
         # self.Head.next = old_Head
-        
+
         """
         Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
         """
-        
 
     def addAtTail(self, val: int):
 
         if self.Head.val == None:
             self.Head.val = val
             return
-        
+
         newNode = Node(val)
         current = self.Head
         while True:
@@ -74,26 +72,22 @@ class MyLinkedList:
                 current.next = newNode
                 break
 
-
-
             current = current.next
-        
+
         """
         Append a node of value val to the last element of the linked list.
         """
 
     def ShowAll(self):
 
-        values = []    
+        values = []
         current = self.Head
 
-        
         count = 0
 
         while True:
 
-            count +=1
-            
+            count += 1
 
             values.append(current.val)
             current = current.next
@@ -102,8 +96,6 @@ class MyLinkedList:
 
         return values
 
-    
-    
     def addAtIndex(self, index: int, val: int):
         loop_index = 0
         newNode = Node(val)
@@ -112,42 +104,38 @@ class MyLinkedList:
 
             self.addAtHead(val)
             return
-        
+
         if self.Head.val != None:
             current = self.Head
             # while current.next != None:
             while True:
 
-                if loop_index!=index-1:
+                if loop_index != index-1:
                     current = current.next
-                    loop_index+=1
+                    loop_index += 1
                 else:
-                    #Node before index Node
+                    # Node before index Node
                     beforeCurrent = current
                     current = current.next
 
-                    #Storing the address of next node in new node
+                    # Storing the address of next node in new node
                     newNode.next = current
 
                     beforeCurrent.next = newNode
                     break
 
-                
                 if current == None:
                     break
-
 
         elif self.Head.val == None and index == 0:
             self.Head = newNode
 
-
         """
         Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
         """
-        
 
     def deleteAtIndex(self, index: int):
-        
+
         # print("Before Delete")
         # print(self.ShowAll())
 
@@ -156,15 +144,14 @@ class MyLinkedList:
 
         if index == 0:
             self.Head = self.Head.next
-            return 
-
+            return
 
         while True:
 
             if count_index != index-1:
                 current = current.next
-                count_index+=1
-                                
+                count_index += 1
+
             elif current.next == None:
                 # print("Here")
                 break
@@ -177,3 +164,90 @@ class MyLinkedList:
                 break
         # print("Afer Del")
         # print(self.ShowAll())
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+obj = MyLinkedList()
+obj.addAtHead(0)
+obj.addAtTail(1)
+obj.addAtTail(2)
+obj.Head.next.next.next = obj.Head
+
+
+class Solution:
+    def hasCycle(self, head: MyLinkedList) -> bool:
+        if head == None:
+            return False
+        fast = slow = head
+        while fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
+
+
+s = Solution()
+print(s.hasCycle(obj.Head))
+
+obj = MyLinkedList()
+obj.addAtHead(0)
+obj.addAtTail(1)
+obj.addAtTail(2)
+print(s.hasCycle(obj.Head))
+
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        if head is None:
+            return None
+        fast = slow = entry = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            # print('slow at ', slow.val, ' fast at ', fast.val)
+            if fast == slow:
+                # print('meet at ', slow.val)
+                while(slow != entry):
+                    # print('    slow at ', slow.val, ' entry at ', entry.val)
+                    entry = entry.next
+                    slow = slow.next
+                return entry
+
+        return None
+
+# circle = MyLinkedList()
+# circle.addAtTail(1)
+# circle.addAtTail(2)
+# circle.addAtTail(3)
+# circle.addAtTail(4)
+# circle.addAtTail(5)
+# circle.Head.next.next.next.next.next=circle.Head.next
+
+# print(circle.Head.next.next.next.next.val)
+
+# circle.Head.next.next.next.next.next.val
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+circle = ListNode(1)
+circle.next = ListNode(2)
+circle.next.next = ListNode(3)
+circle.next.next.next = ListNode(4)
+circle.next.next.next.next = ListNode(5)
+circle.next.next.next.next.next = ListNode(6)
+circle.next.next.next.next.next.next = circle.next.next.next.next
+print(circle.next.next.next.next.next.val)
+
+s = Solution()
+print(s.detectCycle(circle).val)
